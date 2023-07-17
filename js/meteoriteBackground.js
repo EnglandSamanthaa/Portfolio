bd5c1b0041db37376e4189ee3b75df249981a7fb
Samantha England <64668742+EnglandSamanthaa@users.noreply.github.com>
2023-07-17 17:48:50 -0500

    Add files via upload

---
+++ js/meteoriteBackground.js
@@ -0,0 +1,76 @@
+window.addEventListener('load', function() {
+    const homeCanvas = document.getElementById('homeScreenCanvas');
+    const introHThree = document.getElementById('introHThree');
+    const name = document.getElementById('name');
+    const introParagraph = document.getElementById('introParagraph');
+    const ctxHome = homeCanvas.getContext('2d');
+    homeCanvas.width = window.innerWidth;
+    homeCanvas.height = window.innerHeight;
+    ctxHome.strokeStyle = "white";
+    const gradient = ctxHome.createLinearGradient(0,0, homeCanvas.width, homeCanvas.height);
+    gradient.addColorStop(0, 'blue');
+    gradient.addColorStop(0.5, 'white');
+    gradient.addColorStop(1, 'purple');
+    ctxHome.fillStyle = gradient;
+
+    class Particle {
+        constructor(effect) {
+            this.effect = effect;
+            this.radius = Math.random() * 3;
+            this.x = this.radius + Math.random() * (this.effect.width );
+            this.y = Math.random() * (this.effect.height );
+            this.vx = Math.random() * 4 - 3;
+            this.vy = Math.random() * 4 - 3;
+            
+        }
+        draw(ctxHome){
+            ctxHome.beginPath();
+            ctxHome.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctxHome.fill();
+        }
+        update(){
+            this.x += this.vx;
+            this.y += this.vy;
+
+            if (this.x > this.effect.width || this.x < 0) this.vx *= -1;
+            if (this.y > this.effect.height || this.y < 0) this.vy *= -1;
+        }
+
+    }
+
+    class Effect {
+        constructor(homeCanvas){
+            this.homeCanvas = homeCanvas;
+            this.width = this.homeCanvas.width;
+            this.height = this.homeCanvas.height;
+            this.particles = [];
+            this.numberOfParticles = 420;
+            this.createParticles();
+        }
+
+        createParticles(){
+            for(let i = 0; i < this.numberOfParticles; i++){
+                this.particles.push(new Particle(this));
+            }
+        }
+        handleParticles(){
+            this.particles.forEach(particle => {
+                particle.draw(ctxHome);
+                particle.update();
+            });
+        }
+    }
+
+    const effect = new Effect(homeCanvas);
+    effect.handleParticles(ctxHome);
+
+    function animate(){
+        ctxHome.clearRect(0, 0, homeCanvas.width, homeCanvas.height);
+        effect.handleParticles(ctxHome);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
+
+
